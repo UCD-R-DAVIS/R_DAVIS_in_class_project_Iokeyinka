75b29f6b32ebd6421c551a2064ec1a2ad9516b6e
Iokeyinka <iaokeyinka@ucdavis.edu>
2024-12-05 13:12:47 -0800

    COMMIT!

---
+++ Assignment_9_R.R
@@ -0,0 +1,33 @@
+surveys <- read.csv("data/portal_data_joined.csv")
+mloa <- read.csv("https://raw.githubusercontent.com/ucd-cepb/R-DAVIS/master/data/mauna_loa_met_2001_minute.csv")
+
+
+#part 1
+for(i in unique(surveys$taxa)){
+  mytaxon <- surveys[surveys$taxa == i,]
+  longestnames <- mytaxon[nchar(mytaxon$species) == max(nchar(mytaxon$species)),] %>% select(species)
+  print(paste0("The longest species name(s) among ", i, "s is/are: "))
+  print(unique(longestnames$species))
+}
+
+#part 2
+mycols <- mloa %>% select("windDir","windSpeed_m_s","baro_hPa","temp_C_2m","temp_C_10m","temp_C_towertop","rel_humid", "precip_intens_mm_hr")
+mycols %>% map(max, na.rm = T)
+
+#part 3
+C_to_F <- function(x){
+  x * 1.8 + 32
+}
+
+mloa$temp_F_2m <- C_to_F(mloa$temp_C_2m)
+mloa$temp_F_10m <- C_to_F(mloa$temp_C_10m)
+mloa$temp_F_towertop <- C_to_F(mloa$temp_C_towertop)
+
+#Bonus:
+mloa %>% select(c("temp_C_2m", "temp_C_10m", "temp_C_towertop")) %>% map_df(C_to_F) %>% rename("temp_F_2m"="temp_C_2m", "temp_F_10m"="temp_C_10m", "temp_F_towertop"="temp_C_towertop") %>% cbind(mloa)
+
+#challenge
+surveys$genusspecies <- lapply(1:length(surveys$species), function(i){
+  paste0(surveys$genus[i], " ", surveys$species[i])
+})
+
